76c5d28489438165540843e600217cf58bc27aad
Jamie-nyanchi <80130176+JamieNyanchi@users.noreply.github.com>
2023-05-17 23:42:21 +0900

    Tweak BWR
    Todo...

--- BWR/WorldRankings_BWR.lua
+++ BWR/WorldRankings_BWR.lua
@@ -10,10 +10,15 @@ print("Loading WorldRankings_BWR.lua from Better World Rankings version "..Globa
 BASE_PopulateOverallInstance = PopulateOverallInstance;
 BASE_PopulateOverallTeamIconInstance = PopulateOverallTeamIconInstance;
 BASE_PopulateOverallPlayerIconInstance = PopulateOverallPlayerIconInstance;
+BASE_PopulateScienceProgressMeters = PopulateScienceProgressMeters;
 BASE_GatherCultureData = GatherCultureData;
 BASE_PopulateCultureInstance = PopulateCultureInstance;
 BASE_ViewCulture = ViewCulture;
+BASE_PopulateTeamInstanceShared = PopulateTeamInstanceShared;
 
+-- Expansions check
+local bIsRiseAndFall:boolean = Modding.IsModActive("1B28771A-C749-434B-9053-D1380C553DE9"); -- Rise & Fall
+local bIsGatheringStorm:boolean = Modding.IsModActive("4873eb62-8ccc-4574-b784-dda455e74e68"); -- Gathering Storm
 
 -- calculate tourism needed to attract one visiting tourist
 local m_iTourismForOne:number = GlobalParameters.TOURISM_TOURISM_TO_MOVE_CITIZEN * PlayerManager.GetWasEverAliveMajorsCount();
@@ -113,6 +118,17 @@ function PopulateOverallTeamIconInstance(instance:table, teamData:table, iconSiz
     
 	-- new fields
 	local score1:number, score2:number = Round(teamData.FirstTeamTiebreakScore, 0), Round(teamData.SecondTeamTiebreakScore, 0);
+    local victoryData = g_victoryData[victoryType];
+    local player = {};
+    for _, playerData in pairs(teamData.PlayerData) do
+        player = playerData.Player;
+        break;
+    end
+    local firstTeamTiebreakSummary = Locale.Lookup((victoryData.Primary or victoryData).GetText(player), score1);
+    local secondTeamTiebreakSummary = Locale.Lookup((victoryData.Secondary or victoryData).GetText(player), score2);
+    -- tooltips are the same for all
+    instance.Line1:SetToolTipString(firstTeamTiebreakSummary);
+	instance.Line2:SetToolTipString(secondTeamTiebreakSummary);
     -- formatting depends on the victory type
     if     victoryType == "VICTORY_TECHNOLOGY" then
         instance.Line1:SetText(tostring(score1));
@@ -154,8 +170,8 @@ function PopulateOverallPlayerIconInstance(instance:table, victoryType:string, t
 	-- Take the player ID from the first team member who should be the only team member
 	local playerID:number = Teams[teamData.TeamID][1];
 	local playerData:table = teamData.PlayerData[playerID];
-	local score1:number, score2:number = Round(playerData.FirstTiebreakScore, 0), Round(playerData.SecondTiebreakScore, 0);
 	if playerData ~= nil then
+        local score1:number, score2:number = Round(playerData.FirstTiebreakScore, 0), Round(playerData.SecondTiebreakScore, 0);
 		-- tooltips are the same for all
 		instance.Line1:SetToolTipString(playerData.FirstTiebreakSummary);
 		instance.Line2:SetToolTipString(playerData.SecondTiebreakSummary);
@@ -176,7 +192,9 @@ function PopulateOverallPlayerIconInstance(instance:table, victoryType:string, t
 			instance.Line1:SetText(tostring(score1));
 			instance.Line2:SetText("[COLOR_FaithDark]"..tostring(score2).."[ENDCOLOR]");
 		elseif victoryType == "VICTORY_DIPLOMATIC" then
+            local numNeeded:number = GlobalParameters.DIPLOMATIC_VICTORY_POINTS_REQUIRED;
 			instance.Line1:SetText(tostring(score1));
+            instance.Line1:SetToolTipString(Locale.Lookup("LOC_WORLD_RANKINGS_DIPLOMATIC_POINTS_TT", score1, numNeeded));
 			instance.Line2:SetHide(true);
 		else
 			instance.Line1:SetText(tostring(score1));
@@ -185,18 +203,287 @@ function PopulateOverallPlayerIconInstance(instance:table, victoryType:string, t
 	end
 end
 
+-- Constants copied from base file for use here
+local PADDING_VICTORY_LABEL_UNDERLINE:number = 90;
+local SIZE_VICTORY_ICON_SMALL:number = 64;
+local TEAM_ICON_SIZE_TOP_TEAM:number = 38;
+local TEAM_RIBBON_SIZE_TOP_TEAM:number = 53;
+local SIZE_OVERALL_TOP_PLAYER_ICON:number = 48;
+local TEAM_ICON_SIZE:number = 28;
+local TEAM_RIBBON_SIZE:number = 44;
+local SIZE_OVERALL_PLAYER_ICON:number = 36;
+local DATA_FIELD_OVERALL_PLAYERS_IM:string = "OverallPlayersIM";
+
 local SIZE_OVERALL_BG_HEIGHT:number = 95;
 local SIZE_OVERALL_INSTANCE:number = 75;
 
 function PopulateOverallInstance(instance:table, victoryType:string, typeText:string)
+
+	local victoryInfo:table= GameInfo.Victories[victoryType];
+	local numIcons = 0;
+
+	instance.VictoryLabel:SetText(Locale.ToUpper(Locale.Lookup(victoryInfo.Name)));
+	instance.VictoryLabelUnderline:SetSizeX(instance.VictoryLabel:GetSizeX() + PADDING_VICTORY_LABEL_UNDERLINE);
+	
+	local icon:string;
+	local color:number;
+	if typeText ~= nil then
+		icon = "ICON_VICTORY_" .. typeText;
+		color = UI.GetColorValue("COLOR_VICTORY_" .. typeText);
+	else
+		icon = victoryInfo.Icon or ICON_GENERIC;
+		color = UI.GetColorValue("White");
+	end
+	instance.VictoryBanner:SetColor(color);
+	instance.VictoryLabelGradient:SetColor(color);
+
+	if icon ~= nil then
+		local textureOffsetX:number, textureOffsetY:number, textureSheet:string = IconManager:FindIconAtlas(icon, SIZE_VICTORY_ICON_SMALL);
+		if(textureSheet == nil or textureSheet == "") then
+			UI.DataError("Could not find icon in PopulateOverallInstance: icon=\""..icon.."\", iconSize="..tostring(SIZE_VICTORY_ICON_SMALL));
+		else
+			instance.VictoryIcon:SetTexture(textureOffsetX, textureOffsetY, textureSheet);
+			instance.VictoryIcon:SetHide(false);
+		end
+	else
+		instance.VictoryIcon:SetHide(true);
+	end
+
+	-- Cache victory data to avoid table access within loops
+	local victoryData = g_victoryData[victoryType];
+
+	-- Team tiebreaker score functions
+	local averageScores = function(playerData, playerCount, scoreKey)
+		-- Add player scores
+		local score:number = 0;
+		for _, player in pairs(playerData) do
+			score = score + player[scoreKey];
+		end
+		-- Divide by player count
+		return score / playerCount;
+	end;
+
+	-- Gather team data
+	local teamIDs = GetAliveMajorTeamIDs();
+
+	local teamData:table = {};
+	for _, teamID in ipairs(teamIDs) do
+		local team = Teams[teamID];
+		if(team ~= nil) then
+			-- If progress is nil, then the team is not capable of earning a victory (ex: city-state teams and barbarian teams).
+			-- Skip any team that is incapable of earning a victory.
+			local progress = Game.GetVictoryProgressForTeam(victoryType, teamID);
+			if(progress ~= nil) then
+
+				-- PlayerData
+				local playerData:table = {};
+				local playerCount:number = 0;
+                local everAliveCount:number = 0;
+				local teamGenericScore = 0;
+
+				for i, playerID in ipairs(team) do
+					if IsAliveAndMajor(playerID) then
+						local pPlayer:table = Players[playerID];
+
+						local firstTiebreaker:table = victoryData.Primary or victoryData;
+						local secondTiebreaker:table = victoryData.Secondary or victoryData;
+						local primaryScore:number = firstTiebreaker.GetScore(pPlayer);
+						local secondaryScore:number = secondTiebreaker.GetScore(pPlayer);					
+						local additionalSummary:string = (victoryData.AdditionalSummary and victoryData.AdditionalSummary(pPlayer)) or "";
+
+						local genericScore = pPlayer:GetScore();
+
+						-- Team score is calculated as the highest individual score.
+						teamGenericScore = math.max(teamGenericScore, genericScore);
+
+						playerData[playerID] = {
+							Player = pPlayer,
+							GenericScore = genericScore,
+							FirstTiebreakScore = primaryScore,
+							SecondTiebreakScore = secondaryScore,
+							FirstTiebreakSummary = Locale.Lookup(firstTiebreaker.GetText(pPlayer), Round(primaryScore, 1)),
+							SecondTiebreakSummary = Locale.Lookup(secondTiebreaker.GetText(pPlayer), Round(secondaryScore, 1)),							
+							AdditionalSummary = Locale.Lookup(additionalSummary);
+						};
+
+						playerCount = playerCount + 1;
+					end
+                    
+                    if (Players[playerID]:IsMajor()) then
+                        everAliveCount = everAliveCount + 1;
+                    end
+				end
+
+				-- Team Data
+				table.insert(teamData, {
+					TeamID = teamID,
+					TeamScore = progress,
+					TeamProgress = progress,
+					TeamGenericScore = teamGenericScore,
+					PlayerData = playerData,
+					PlayerCount = playerCount,
+                    EverAliveCount = everAliveCount,
+					FirstTeamTiebreakScore = averageScores(playerData, playerCount, "FirstTiebreakScore");
+					SecondTeamTiebreakScore = averageScores(playerData, playerCount, "SecondTiebreakScore");
+				});
+			end
+		end
+	end
+
+	-- Sort teams by score
+	table.sort(teamData, function(a, b)
+		if (a.TeamProgress ~= b.TeamProgress) then
+			return a.TeamProgress > b.TeamProgress;
+		elseif(a.FirstTeamTiebreakScore ~= b.FirstTeamTiebreakScore) then
+			return a.FirstTeamTiebreakScore > b.FirstTeamTiebreakScore;
+		elseif(a.SecondTeamTiebreakScore ~= b.SecondTeamTiebreakScore) then
+			return a.SecondTeamTiebreakScore > b.SecondTeamTiebreakScore;
+		elseif(a.TeamGenericScore ~= b.TeamGenericScore) then
+			return a.TeamGenericScore > b.TeamGenericScore;
+		else
+			return a.TeamID < b.TeamID;
+		end
+	end);
+
+	-- Handle case where this victory type is not completable by any team.
+	-- This can happen with Global Thermonuclear War's Proxy War victory if there are no city states to conquer.
+	if(#teamData < 1) then
+			instance.VictoryPlayer:SetText("");
+			instance.VictoryLeading:SetText(Locale.Lookup("LOC_WORLD_RANKINGS_VICTORY_DISABLED"));
+			-- Make the whole box look plain, since the victory is out of reach
+			instance.TeamRibbon:SetHide(true);
+			instance.TopPlayer:SetHide(true);
+			instance.CivIcon:SetHide(true);
+			instance.CivIconFaded:SetHide(true);
+			instance.CivIconBacking:SetHide(true);
+			instance.CivIconBackingRing:SetHide(true);
+			instance.CivIconBackingFaded:SetHide(true);
+			instance.VictoryLabelGradient:SetHide(true); 
+			instance.VictoryBanner:SetHide(true); 
+			instance.VictoryIcon:SetHide(true); 
+		return;
+	end
+
+	-- Ensure we have Instance Managers for the player meters
+	local playersIM:table = instance[DATA_FIELD_OVERALL_PLAYERS_IM];
+	if(playersIM == nil) then
+		playersIM = InstanceManager:new("OverallPlayerInstance", "CivIconBackingFaded", instance.PlayerStack);
+		instance[DATA_FIELD_OVERALL_PLAYERS_IM] = playersIM;
+	end
+	playersIM:ResetInstances();
+
+	-- Populate top team/player icon
+	if teamData[1].EverAliveCount > 1 then
+		PopulateOverallTeamIconInstance(instance, teamData[1], TEAM_ICON_SIZE_TOP_TEAM, TEAM_RIBBON_SIZE_TOP_TEAM);
+	else
+		PopulateOverallPlayerIconInstance(instance, victoryType, teamData[1], SIZE_OVERALL_TOP_PLAYER_ICON);
+	end
+	numIcons = numIcons + 1;
+
+	-- Populate other team/player icons
+	if #teamData > 1 then
+		for i = 2, #teamData, 1 do
+			local playerInstance:table = playersIM:GetInstance();
+			if teamData[i].EverAliveCount > 1 then
+				PopulateOverallTeamIconInstance(playerInstance, teamData[i], TEAM_ICON_SIZE, TEAM_RIBBON_SIZE);
+			else
+				PopulateOverallPlayerIconInstance(playerInstance, victoryType, teamData[i], SIZE_OVERALL_PLAYER_ICON);
+			end
+			numIcons = numIcons + 1;
+		end
+	end
+
+	-- Determine if local player is leading
+	local isLocalPlayerLeading:boolean = false;
+	local leadingTeam:table = teamData[1];
+	for playerID, data in pairs(teamData[1].PlayerData) do
+		if playerID == g_LocalPlayerID then
+			isLocalPlayerLeading = true;
+		end
+	end
+
+	-- Populate leading and local player labels
+	if isLocalPlayerLeading then
+		-- You or your team is leading
+		instance.VictoryPlayer:SetText("");
+		if teamData[1].EverAliveCount > 1 then
+			instance.VictoryLeading:SetText(Locale.Lookup("LOC_WORLD_RANKINGS_FIRST_PLACE_TEAM_SIMPLE"));
+		else
+			instance.VictoryLeading:SetText(Locale.Lookup("LOC_WORLD_RANKINGS_FIRST_PLACE_YOU_SIMPLE"));
+		end
+	else
+		-- Set top team/player text
+		local topName:string = "";
+		if teamData[1].EverAliveCount > 1 then
+			topName = Locale.Lookup("LOC_WORLD_RANKINGS_TEAM", GameConfiguration.GetTeamName(teamData[1].TeamID) + 1);
+		else
+			local topPlayerID:number = Teams[teamData[1].TeamID][1];
+			if(g_LocalPlayer == nil or g_LocalPlayer:GetDiplomacy():HasMet(topPlayerID))then
+				topName = Locale.Lookup(GameInfo.Civilizations[PlayerConfigurations[Teams[teamData[1].TeamID][1]]:GetCivilizationTypeID()].Name);
+			else
+				topName = LOC_UNKNOWN_CIV;
+			end
+		end
+		instance.VictoryLeading:SetText(Locale.Lookup("LOC_WORLD_RANKINGS_FIRST_PLACE_OTHER_SIMPLE", topName));
+
+		-- Set local team/player text
+		local isVictoryPlayerSet:boolean = false;
+		for teamPosition, team in ipairs(teamData) do
+			for playerID, data in pairs(team.PlayerData) do
+				if playerID == g_LocalPlayerID then
+					local localPlayerPositionText:string = Locale.Lookup("LOC_WORLD_RANKINGS_" .. teamPosition .. "_PLACE");
+					local localPlayerDescription:string = "";
+
+					if team.EverAliveCount > 1 then
+						localPlayerDescription = Locale.Lookup("LOC_WORLD_RANKINGS_OTHER_PLACE_TEAM_SIMPLE", localPlayerPositionText);
+					else
+						localPlayerDescription = Locale.Lookup("LOC_WORLD_RANKINGS_OTHER_PLACE_SIMPLE", localPlayerPositionText);
+					end
+
+					instance.VictoryPlayer:SetText(localPlayerDescription);
+					isVictoryPlayerSet = true;
+				end
+			end
+		end
+		if (not isVictoryPlayerSet) then
+			instance.VictoryPlayer:SetText("");		
+		end
+	end
+
+	--instance.ButtonBG:SetSizeY(SIZE_OVERALL_BG_HEIGHT + math.max(instance.PlayerStack:GetSizeY(), SIZE_OVERALL_INSTANCE * ((numIcons / 9) + 1)));
+
 	--print("FUN PopulateOverallInstance()", victoryType, typeText);
-	BASE_PopulateOverallInstance(instance, victoryType, typeText);
+	--BASE_PopulateOverallInstance(instance, victoryType, typeText);
 	
 	-- this is just to resize the instance properly
 	local numIcons:number = PlayerManager.GetAliveMajorsCount() - 1; -- max 9 in one line
 	local numRows:number = math.floor(numIcons/9); -- full rows
 	if numIcons > numRows * 9 then numRows = numRows + 1; end -- partial row
 	instance.ButtonBG:SetSizeY(SIZE_OVERALL_BG_HEIGHT + SIZE_OVERALL_INSTANCE * numRows);
+end
+
+
+
+-- ===========================================================================
+-- SCIENCE
+
+function PopulateScienceProgressMeters(instance:table, progressData:table)
+    BASE_PopulateScienceProgressMeters(instance, progressData);
+    
+    -- Display progress on Exoplanet Expedition
+    if (bIsGatheringStorm) then
+        if ((progressData.projectProgresses[4] >= progressData.projectTotals[4]) and (progressData.projectTotals[4] ~= 0)) then
+            local pPlayer = Players[progressData.playerID];
+            local lightYears = pPlayer:GetStats():GetScienceVictoryPoints();
+            local lightYearsPerTurn = pPlayer:GetStats():GetScienceVictoryPointsPerTurn();
+            local totalLightYears = g_LocalPlayer:GetStats():GetScienceVictoryPointsTotalNeeded();
+            if lightYears > totalLightYears then
+                lightYears = totalLightYears;
+            end
+
+            instance.ObjBG_5:SetToolTipString(Locale.Lookup("LOC_WORLD_RANKINGS_SCIENCE_IS_MOVING", lightYearsPerTurn).."[NEWLINE]"..Locale.Lookup("LOC_WORLD_RANKINGS_SCIENCE_HAS_MOVED", lightYears, totalLightYears));
+        end
+    end
 end
 
 
@@ -293,6 +580,9 @@ function GatherCultureData()
 				playerData.TurnsTillNext = math.floor(playerData.TurnsTillNext) + 1;
 			end
 		end
+        
+        -- Sort players within teams
+        table.sort(teamData.PlayerData, function(a, b) return a.NumVisitingUs / a.NumRequiredTourists > b.NumVisitingUs / b.NumRequiredTourists; end);
 	end
 	
 	return data;
@@ -390,6 +680,17 @@ function PopulateScoreInstance(instance:table, playerData:table)
 	else
 		instance.ScoreX:SetHide(true);
 	end
+end
+
+
+-- ===========================================================================
+-- SHARED
+
+function PopulateTeamInstanceShared(instance:table, teamID:number)
+    BASE_PopulateTeamInstanceShared(instance, teamID);
+
+    -- Update team name
+	instance.TeamName:SetText(Locale.Lookup("LOC_WORLD_RANKINGS_TEAM", GameConfiguration.GetTeamName(teamID) + 1));
 end
 
 
